b20ead1f3b018b30e733ec34f9e9e8819a0d0eff
Vanya <Vanyanya112321@yandex.ru>
2025-11-06 13:24:26 +0300

    Проблемы со скоростью двигателя

--- main/main_config.h
+++ main/main_config.h
@@ -14,18 +14,18 @@
 #define WIFI_AP_PASS            ""
 #define WIFI_AP_CHANNEL         (0)
 
-// #define DEFAULT_WIFI_STA_SSID   "Aud112"
-#define DEFAULT_WIFI_STA_SSID   "RT-GPON-F6ED"
+// #define DEFAULT_WIFI_STA_SSID   "HUAWEI-F1CD4X"
+#define DEFAULT_WIFI_STA_SSID   "Aud112"
+// #define DEFAULT_WIFI_STA_SSID   "RT-GPON-F6ED"
 // #define DEFAULT_WIFI_STA_SSID   "Nadya 5 g"
-
-// #define DEFAULT_WIFI_STA_PASS   "justapassword"
-#define DEFAULT_WIFI_STA_PASS   "yPYAEyYsUE"
+// #define DEFAULT_WIFI_STA_PASS   "jadcrandall"
+#define DEFAULT_WIFI_STA_PASS   "justapassword"
+// #define DEFAULT_WIFI_STA_PASS   "yPYAEyYsUE"
 // #define DEFAULT_WIFI_STA_PASS   "25049025"
 
-// #define DEFAULT_STATIC_IP_ADDR  "192.168.1.10"
-#define DEFAULT_STATIC_IP_ADDR  "192.168.0.10"
+#define DEFAULT_STATIC_IP_ADDR  "192.168.1.10"
+// #define DEFAULT_STATIC_IP_ADDR  "192.168.0.10"
 // #define DEFAULT_STATIC_IP_ADDR  "192.168.0.50"
-
 #define STATIC_NETMASK_ADDR     "255.255.255.0"
 #define STATIC_GW_ADDR          "192.168.1.1"
 // #define STATIC_GW_ADDR          "192.168.0.10"
@@ -50,15 +50,18 @@
 #define STR_CMD_READ_FLASH      "restore_from_flash"
 #define STR_CMD_WRITE_FLASH     "save_to_flash"
 
-#define PIN_STEP                (32)
-#define PIN_DIR                 (33)
-#define PIN_STOP                (15)
-#define PIN_LED                 (2)
-#define PIN_A                   (25)
-#define PIN_B                   (26)
+#define PIN_STEP                 (32)
+#define PIN_DIR                  (33)
 
-#define DUTY_RESOLUTION_BIT     (8)
+#define PIN_STOP                 (15)
+
+#define PIN_LED                  (2)
+
+#define PIN_A                    (25)
+#define PIN_B                    (26)
+
+
+#define DEFAULT_STEP_PERIOD_US  (1000)
+#define DEFAULT_STEPS           (3000)
 #define DEFAULT_PAUSE           (100)
 #define DEFAULT_CALIBRATION_TIMEOUT (100)
-#define GRAPH_ARRAY_SIZE        (200)
-
