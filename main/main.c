91a243cc50c7989b3baa7f5847d496d3a13b433c
jadcr <jad.crandall@mail.ru>
2025-11-10 16:28:23 +0300

    aaaaa

--- main/main.c
+++ main/main.c
@@ -9,24 +9,25 @@
 #include <sys/param.h>
 #include "sdkconfig.h"
 
-#define HOST            SPI2_HOST
+#define HOST            SPI3_HOST
 #define PIN_NUM_MISO    19
 #define PIN_NUM_MOSI    23
 #define PIN_NUM_CLK     18
 #define PIN_NUM_SS      5
 
 
-esp_err_t lcd_data(spi_device_handle_t spi, const uint8_t *data, int len)
+esp_err_t data(spi_device_handle_t spi, const uint8_t *data, int len)
 {
     esp_err_t ret;
     spi_transaction_t t;
-    // if (len == 0){
-    //     return;
-    // }
-    memset(&t, 0, sizeof(t));
+    t.flags = 0;
+    t.cmd = 0x01;
+    t.addr = 0;
     t.length = len * 8;
     t.tx_buffer = data;
+    t.rx_buffer = data;
     t.user = (void*)1;
+    memset(&t, 0, sizeof(t));
     ret = spi_device_polling_transmit(spi, &t);
     //assert(ret == ESP_OK);
     return ret;
@@ -42,33 +43,53 @@ spi_bus_config_t buscfg = {
     .sclk_io_num = PIN_NUM_CLK,
     .quadhd_io_num = -1,
     .quadwp_io_num = -1,
-    .max_transfer_sz = 4 * 320 * 2 + 8
+    .max_transfer_sz = 64
 };
 spi_device_interface_config_t devcfg = {
     #ifdef CONFIG_OVERCLOCK
     .clock_speed_hz = 26 * 1000 * 1000,
     #else
-    .clock_speed_hz = 10 * 1000 * 1000,
+    .clock_speed_hz = 1 * 1000 * 1000,
     #endif
     .mode = 0,
     .spics_io_num = PIN_NUM_SS,
-    .queue_size = 7
+    .queue_size = 1,
+    .command_bits = 8,
+    .address_bits = 0,
+    .dummy_bits = 0,
+
 };
     ret = spi_bus_initialize(HOST, &buscfg, SPI_DMA_CH_AUTO);
     ESP_ERROR_CHECK(ret);
     ret = spi_bus_add_device(HOST, &devcfg, &spi);
     ESP_ERROR_CHECK(ret);
-    while(1)
+
+        while(1)
     {
-        uint8_t test_buf[64] = "";
-        for (int i = 0; i < 64; i++)
-        {
-            ret = lcd_data(spi, &test_buf[i], 8);
-            ESP_ERROR_CHECK(ret);
-            ESP_LOGI("transmit", "%s", test_buf);
+        uint8_t test_buf[8];
+        ESP_LOGI("MISO", "%02X %02X %02X %02X %02X %02X %02X %02X", test_buf[0],test_buf[1],test_buf[2],test_buf[3],test_buf[4],test_buf[5],test_buf[6],test_buf[7]);
+
+
+        //ret = data(spi, test_buf, sizeof(test_buf));
+        
+        if (ret == ESP_OK) {
+            // ESP_LOGI("MOSI", "%s", test_buf);
+            
+            ESP_LOGI("MISO", "%02X %02X %02X %02X %02X %02X %02X %02X", test_buf[0],test_buf[1],test_buf[2],test_buf[3],test_buf[4],test_buf[5],test_buf[6],test_buf[7]);
         }
+        
+        vTaskDelay(pdMS_TO_TICKS(1000));  
+    }
+
+    /*while(1)
+    {
+        uint8_t test_buf[8];
+        ret = lcd_data(spi, test_buf, sizeof(test_buf));
+        ESP_ERROR_CHECK(ret);
+        ESP_LOGI("transmit", "%s", test_buf);
         vTaskDelay(500);
     }
+    */
 
 }
 
